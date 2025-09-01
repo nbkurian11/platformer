4ec9c51b3de856fc3923d257e46285ff8d970db9
Noel Kurian <nbkurian11@gmail.com>
2025-09-01 11:29:12 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((640,640))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
